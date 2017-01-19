e0aaf0ba3cea5e1e87f1286d4f2bb4bfc7112ad0
Tim <timothy.d.butterworth@gmail.com>
2017-01-18 20:10:28 -0500

    Added fields example

---
+++ src/com/example/fields/FieldExampleObject.java
@@ -0,0 +1,7 @@
+package com.example.fields;
+
+public class FieldExampleObject {
+    public String comment;
+    public String name;
+    public Integer age;
+}
--- src/com/example/runner/Runner.java
+++ src/com/example/runner/Runner.java
@@ -1,7 +1,18 @@
 package com.example.runner;
 
+import com.example.fields.FieldExampleObject;
+
 public class Runner {
     public static void main(String[] args) {
+        //fields
+        FieldExampleObject fieldExampleObject = new FieldExampleObject();
+
+        //"this"
+
+        //methods
+
+        //constructors
+
         //instance vs class
 
         //visibility
