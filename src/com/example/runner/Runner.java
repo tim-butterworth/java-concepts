ee626f3449926d6c3b4e74851650339000ba822d
Tim <timothy.d.butterworth@gmail.com>
2017-01-18 20:04:56 -0500

    initial commit

---
+++ src/com/example/runner/Runner.java
@@ -0,0 +1,11 @@
+package com.example.runner;
+
+public class Runner {
+    public static void main(String[] args) {
+        //instance vs class
+
+        //visibility
+
+        //inheritance
+    }
+}
